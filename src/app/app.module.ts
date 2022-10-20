a80fe5fb8391ecae279922826f60213bc5163f3a
avinash br <avinash.shidle@gmail.com>
2022-10-20 18:50:24 +0530

    mic changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpeechToTextComponent } from './speech-to-text/speech-to-text.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { SpeechToTextComponent } from './speech-to-text/speech-to-text.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
