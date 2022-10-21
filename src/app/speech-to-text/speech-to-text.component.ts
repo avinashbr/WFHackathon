e2982fcca3b941e6cf40fa9d1a3c8ab1e5248390
avinash br <avinash.shidle@gmail.com>
2022-10-21 12:33:10 +0530

    remove template feature added

--- src/app/speech-to-text/speech-to-text.component.ts
+++ src/app/speech-to-text/speech-to-text.component.ts
@@ -18,18 +18,18 @@ export class SpeechToTextComponent implements OnInit {
     { id: 2, name: 'Check account balance in account account1' },
     { id: 3, name: 'Open Allen Smith profile in client management' },
     { id: 4, name: 'what is the balance of allen smith savings account' },
-    {id:5,name:'add a hold recommendation to allen smith trade account as XXX'},
-    {id:6,name:'Transfer XXXX from Allen Smith savings account to Brokerage Account'},
+    { id: 5, name: 'add a hold recommendation to allen smith trade account as XXX' },
+    { id: 6, name: 'Transfer XXXX from Allen Smith savings account to Brokerage Account' },
   ];
   displayedColumns: string[] = ['name'];
   listening: boolean = false;
   value: string = 'foo';
   control = new FormControl(this.value);
   updatedValue: string = '';
-  recentlyExecutedCommands:templateModel[]=[];
+  recentlyExecutedCommands: templateModel[] = [];
 
 
-  constructor(public service: VoiceRecognitionService,public snackBar: MatSnackBar,public dialog: MatDialog) {
+  constructor(public service: VoiceRecognitionService, public snackBar: MatSnackBar, public dialog: MatDialog) {
     this.service.init()
   }
 
@@ -41,26 +41,26 @@ export class SpeechToTextComponent implements OnInit {
       width: '500px',
     });
 
-    dialogRef.afterClosed().subscribe((result:string) => {
+    dialogRef.afterClosed().subscribe((result: string) => {
       console.log(result);
-      if(result.length>0){
+      if (result.length > 0) {
         let id = Math.max(...this.templateList.map(o => o.id));
-        this.templateList.push({id:id,name:result});
-        this.templateList=[...this.templateList];
+        this.templateList.push({ id: id, name: result });
+        this.templateList = [...this.templateList];
       }
-     
+
     });
   }
-  
+
   update(editedVal: string) {
     console.log(editedVal);
     this.snackBar.open(editedVal, 'Success', {
       duration: 5000,
-   });
+    });
     let id = Math.max(...this.recentlyExecutedCommands.map(o => o.id));
 
-    this.recentlyExecutedCommands.push({id:id,name:editedVal});
-    this.recentlyExecutedCommands=[...this.recentlyExecutedCommands];
+    this.recentlyExecutedCommands.push({ id: id, name: editedVal });
+    this.recentlyExecutedCommands = [...this.recentlyExecutedCommands];
   }
 
   cancel(index: number) {
@@ -77,6 +77,12 @@ export class SpeechToTextComponent implements OnInit {
     this.recordedMessage = this.service.text;
     this.service.stop()
   }
-  
+  removeTemplate(tl: templateModel) {
+    this.templateList = this.templateList.filter(function( obj ) {
+      return obj !== tl;
+  });
+    this.templateList = [...this.templateList]
+  }
+
 
 }
