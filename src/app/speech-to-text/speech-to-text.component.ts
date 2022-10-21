0a5e4a572bdd1337e669243d032bd45abde09dab
avinash br <avinash.shidle@gmail.com>
2022-10-21 10:59:42 +0530

    some more templates added

--- src/app/speech-to-text/speech-to-text.component.ts
+++ src/app/speech-to-text/speech-to-text.component.ts
@@ -11,14 +11,18 @@ import { VoiceRecognitionService } from '../services/voice-recognition.service';
 export class SpeechToTextComponent implements OnInit {
   recordedMessage: String = '';
   templateList: templateModel[] = [
-    { id:1,name: 'Transfer money from account1 to account2' },
-    { id:1,name: 'Check account balance in account account1' }
+    { id: 1, name: 'Transfer money from account1 to account2' },
+    { id: 2, name: 'Check account balance in account account1' },
+    { id: 3, name: 'Open Allen Smith profile in client management' },
+    { id: 4, name: 'what is the balance of allen smith savings account' },
+    {id:5,name:'add a hold recommendation to allen smith trade account as XXX'},
+    {id:6,name:'Transfer XXXX from Allen Smith savings account to Brokerage Account'},
   ];
   displayedColumns: string[] = ['name'];
   listening: boolean = false;
-  value:string = 'foo';
+  value: string = 'foo';
   control = new FormControl(this.value);
-  updatedValue:string='';
+  updatedValue: string = '';
 
 
   constructor(public service: VoiceRecognitionService) {
@@ -26,14 +30,14 @@ export class SpeechToTextComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+
   }
-  update(editedVal:string) {
+  update(editedVal: string) {
     console.log(editedVal);
     // this.value = 'this.control?.value;'
   }
 
-  cancel(index:number) {
+  cancel(index: number) {
     console.log(index);
 
     // this.control.setValue(this.value);
